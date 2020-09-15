046d8f44d9763392dbede91e1781c2aacab9194a
Jannik Zschiesche <j.zschiesche@21torr.com>
2020-09-15 10:42:56 +0200

    Add implementation

---
+++ src/Bundle/BundleExtension.php
@@ -0,0 +1,54 @@
+<?php declare(strict_types=1);
+
+namespace Torr\RadBundles\Bundle;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+
+/**
+ * Base bundle extension, that can be used without needing a custom class.
+ *
+ * @final
+ */
+class BundleExtension extends Extension
+{
+    private BundleInterface $bundle;
+    private ?string $alias;
+
+
+    public function __construct (
+        BundleInterface $bundle,
+        ?string $alias = null
+    )
+    {
+        $this->bundle = $bundle;
+        $this->alias = $alias;
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    public function load (array $configs, ContainerBuilder $container) : void
+    {
+        $configDir = "{$this->bundle->getPath()}/config";
+
+        if (\is_file("{$configDir}/services.yaml"))
+        {
+            $loader = new YamlFileLoader($container, new FileLocator($configDir));
+            $loader->load("services.yaml");
+        }
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    public function getAlias () : string
+    {
+        return $this->alias ?? parent::getAlias();
+    }
+}
---
+++ src/Bundle/ConfigurableBundleExtension.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types=1);
+
+namespace Torr\RadBundles\Bundle;
+
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+
+/**
+ * Base bundle extension, that receives the parsed config + container builder and can modify
+ * the container accordingly.
+ *
+ * @final
+ */
+class ConfigurableBundleExtension extends BundleExtension
+{
+    private ConfigurationInterface $config;
+    /** @var callable */
+    private $callback;
+
+    /**
+     */
+    public function __construct (
+        BundleInterface $bundle,
+        ConfigurationInterface $config,
+        callable $callback,
+        ?string $alias = null
+    )
+    {
+        parent::__construct($bundle, $alias);
+        $this->config = $config;
+        $this->callback = $callback;
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    public function load (array $configs, ContainerBuilder $container) : void
+    {
+        parent::load($configs, $container);
+
+        $config = $this->processConfiguration($this->config, $configs);
+        ($this->callback)($config, $container);
+    }
+}
